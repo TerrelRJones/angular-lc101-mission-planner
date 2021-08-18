ef3307b9e07c1e0beb8e5389401a5a7b661d22dd
Terrel <TerrelRJones@gmail.com>
2021-08-17 18:55:23 -0700

    Angular studio 2 done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -17,5 +19,25 @@ export class CrewComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  add(name, firstMission){
+    this.crew.push({name, firstMission})
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+  
+  
 
 }
